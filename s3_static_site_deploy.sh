d1596ac403e7a62b62e1c8019a8c432150e177c5
Fletcher Wilkens <fletcher.wilkens1@gmail.com>
2018-03-26 21:13:38 -0700

    have scripts accept args for aws ids/buckets

--- s3_static_site_deploy.sh
+++ s3_static_site_deploy.sh
@@ -1,13 +1,15 @@
-deploystaticsite () {
-  aws s3 sync $1 "s3://$STATIC_SITE_S3_BUCKET"
-  for i in `aws s3 ls s3://$STATIC_SITE_S3_BUCKET --recursive | grep '.html' | awk '{print $4}'`;do
+deploy_static_site () {
+  s3_bucket_path=$2
+  aws s3 sync $1 "s3://$s3_bucket_path"
+  for i in `aws s3 ls s3://$s3_bucket_path --recursive | grep '.html' | awk '{print $4}'`;do
     [ -f "$i" ] || break
     original_file_name=$i
     new_file_name="${original_file_name/.html/}"
-    aws s3 mv "s3://$STATIC_SITE_S3_BUCKET/$original_file_name" "s3://$STATIC_SITE_S3_BUCKET/$new_file_name" --content-type 'text/html'
+    aws s3 mv "s3://$s3_bucket_path/$original_file_name" "s3://$s3_bucket_path/$new_file_name" --content-type 'text/html'
   done
 }
 
-reloadstaticsite () {
-  aws cloudfront create-invalidation --distribution-id $STATIC_SITE_CLOUDFRONT_DISTRO_ID --paths "/*"
+reload_static_site () {
+  cloudfront_distro_id=$1
+  aws cloudfront create-invalidation --distribution-id $cloudfront_distro_id --paths "/*"
 }
