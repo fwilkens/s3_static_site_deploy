4483ac5e1d88f45cb2625ebdde4f90c6ef535943
Fletcher Wilkens <fletcher.wilkens1@gmail.com>
2018-03-26 21:55:07 -0700

    remove if switch checking for local file

--- s3_static_site_deploy.sh
+++ s3_static_site_deploy.sh
@@ -1,8 +1,8 @@
 deploy_static_site () {
   s3_bucket_path=$2
   aws s3 sync $1 "s3://$s3_bucket_path"
+
   for i in `aws s3 ls s3://$s3_bucket_path --recursive | grep '.html' | awk '{print $4}'`;do
-    [ -f "$i" ] || break
     original_file_name=$i
     new_file_name="${original_file_name/.html/}"
     aws s3 mv "s3://$s3_bucket_path/$original_file_name" "s3://$s3_bucket_path/$new_file_name" --content-type 'text/html'
